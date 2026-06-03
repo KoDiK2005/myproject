7968c3526141f8ebf79521ddf54522263514f854
KoDiK2005 <zingotv083@gmail.com>
2026-06-03 11:22:29 +0300

    fix: return 201 on user creation

--- internal/handler/userhandler.go
+++ internal/handler/userhandler.go
@@ -37,5 +37,6 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(user)
 }
